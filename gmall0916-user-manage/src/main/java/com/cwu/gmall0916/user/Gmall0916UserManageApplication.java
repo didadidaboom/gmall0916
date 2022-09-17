f65aba05f5195ff0f3a05cb3ad47e2dc34bea8e0
didadidaboom <chong.wu.sg@outlook.com>
2022-09-18 03:05:42 +0800

    added: user manage

---
+++ gmall0916-user-manage/src/main/java/com/cwu/gmall0916/user/Gmall0916UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.cwu.gmall0916.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0916UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0916UserManageApplication.class, args);
+    }
+
+}
---
+++ gmall0916-user-manage/src/test/java/com/cwu/gmall0916/user/Gmall0916UserManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwu.gmall0916.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmall0916UserManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
